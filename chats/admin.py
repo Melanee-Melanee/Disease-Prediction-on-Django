b09dff5a29f76fa320c9f0457fcf21bedb06d617
Melanee <74653444+Melanee-Melanee@users.noreply.github.com>
2023-04-18 10:10:14 +0330

    Delete chats directory

--- chats/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Chat,Feedback
-# Register your models here.
-
-admin.site.register(Chat)
-admin.site.register(Feedback)
--- chats/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path , include
-from . import views
-
-urlpatterns = [
-
-  path('post_feedback', views.post_feedback, name='post_feedback'),
-  path('get_feedback', views.get_feedback, name='get_feedback')
-   
-]
